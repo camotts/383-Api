581322e0699b88fd2e0184d29e6c917152668fec
Michael Babin <mbabin31@yahoo.com>
2015-02-24 16:59:13 -0600

    Added a accountlogin controller and apkikey controller

---
+++ GameingStoreAPI/Controllers/AccountLoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GamingStoreAPI.DAL;
+using System.Web.Http.Hosting;
+
+
+namespace GamingStoreAPI.Controllers
+{
+    public class AccountLoginController : ApiController
+    {
+
+        [Basic
+        public 
+
+
+    }
+}
---
+++ GameingStoreAPI/Controllers/ApiKeyController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GamingStoreAPI.DAL;
+
+namespace GamingStoreAPI.Controllers
+{
+    public class ApiKeyController : ApiController
+    {
+
+        //Endpoint requirement to acquire apikey from login
+        // with valid email and password.
+        [HttpGet]
+        public string getApiKey()
+        {
+            using (DataContext db = new DataContext()){
+
+                if (User.Identity.IsAuthenticated)
+                {
+                    return db.Users.Find(User.Identity.Name).APIKey;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+
+    }
+}
